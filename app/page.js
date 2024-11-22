896daee8e2bcdfa03ae6516c3ab242261ac7f78f
27janani <sce22is056@sairamtap.edu.in>
2024-11-22 12:08:59 +0530

    context

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
+"use client"
+
+import { useAppContext } from "@/context";
 import React from "react";
 
 const Welcome = () => {
+  const {state} = useAppContext();
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-black">
       <h1 className="text-4xl font-bold text-green-500 mb-4">
-        Welcome to Our App!
+        {state} Our App!
       </h1>
       <p className="text-lg text-gray-600">
         We’re glad to have you here. Enjoy exploring!
