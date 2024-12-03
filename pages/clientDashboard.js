154c8dc1cd61158319ed190cf6647e57025507e7
naveen-2111-dev <naveenrajanm9@gmail.com>
2024-12-03 09:03:18 +0530

    error

--- pages/clientDashboard.js
+++ pages/clientDashboard.js
@@ -34,7 +34,7 @@ const ClientDashboard = () => {
         </div>
         <button
           className="w-full px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600"
-          onClick={() => router.push("/clientdashboard") }
+          onClick={() => router.push("/clientdash") }
         >
           Next
         </button>
