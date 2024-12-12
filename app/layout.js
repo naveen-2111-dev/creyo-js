2780222c3e2cce7f08216008ef2172539361d56b
saransrini003 <saransrini003@gmail.com>
2024-12-12 11:50:08 +0530

    1150

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,18 @@
-// app/layout.js
-import { UserProvider } from "@/context/userContext";
+import Head from "next/head";
+import "./globals.css";
+
+export const metadata = {
+  title: "creyo",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <UserProvider>{children}</UserProvider>
+        <Head>
+          <title>Creyo</title>
+        </Head>
+        {children}
       </body>
     </html>
   );
